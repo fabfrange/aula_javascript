5696e10a9a87fd28986d7e03c5d3b2d7e2788837
fabfrange <fabfrange@hotmail.com>
2020-11-20 01:55:25 -0300

    Meu primeiro js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Fabiano Frange";
+var idade = 33;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo!";
+//alert(nome + " tem " + idade + " anos!");
+//alert(idade+idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
