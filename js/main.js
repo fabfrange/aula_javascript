d4d95f927ab1ca4b01f629a36b8415d558627be3
fabfrange <fabfrange@hotmail.com>
2020-11-21 00:25:15 -0300

    Meu segundo js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,72 @@
-var nome = "Fabiano Frange";
-var idade = 33;
-var idade2 = 10;
-var frase = "Japão é o melhor time do mundo!";
+
+
+
+/*var d=new Date();
+alert(d);
+alert(d.getMonth()+1);
+alert(d.getDay());
+alert(d.getHours());
+*/
+
+
+/*
+var count;
+for(count=0; count<5; count++){
+    //console.log(count);
+    alert(count);
+};
+*/
+
+
+/*
+var count=0;
+while(count < 5){
+    console.log(count);
+    count=count+1;
+};
+*/
+
+
+/*
+var idade=prompt("Qual sua idade");
+if(idade >=18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+
+
+/*var frutas=[{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta={nome:"maça", cor:"vermelha"};
+//console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista=["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+
+//var nome = "Fabiano Frange";
+//var idade = 33;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo!";
 //alert(nome + " tem " + idade + " anos!");
 //alert(idade+idade2);
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase.replace("Japão", "Brasil"));
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
 //alert(frase.replace("Japão", "Brasil"));
-console.log(frase.toUpperCase());
+//console.log(frase.toUpperCase());
